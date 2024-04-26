4a448a3b007f219de091c16a1f15e46054bfd759
YASH YADAV <75584422+ALPHAWINS02@users.noreply.github.com>
2024-04-26 18:51:21 +0530

    Add files via upload

---
+++ src/components/AddGroupButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AddGroupButton.css'; // Import CSS file
+
+const AddGroupButton = ({ onAddGroup }) => {
+    return (
+        <button className="add-group-button" onClick={onAddGroup}>
+            <span className="plus-sign">+</span>
+        </button>
+    );
+};
+
+export default AddGroupButton;
